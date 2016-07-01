58f6ca7e0195186425a159c49b78b2931f906258
Gyusun Yeom <omniavinco@gmail.com>
2016-07-01 22:18:56 +0900

    import hinting

---
+++ index.ts
@@ -0,0 +1,10 @@
+import * as decorator from './src/decorator';
+import * as parser from './src/parser';
+
+export const DBTypes = decorator.DBTypes;
+export const embededField = decorator.embededField;
+export const primaryKey = decorator.primaryKey;
+export const arrayJoinedWith = decorator.arrayJoinedWith;
+export const concreteType = decorator.concretType;
+export const internal = decorator.internal;
+export const parseDefinition = parser.parse;
