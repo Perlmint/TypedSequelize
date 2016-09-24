36de29660918801f7ab08d0c2376a7beea698eca
Gyusun Yeom <omniavinco@gmail.com>
2016-09-24 11:14:25 +0900

    remove built

--- bin/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(function (RelationshipType) {
-    RelationshipType[RelationshipType["OneToMany"] = 0] = "OneToMany";
-    RelationshipType[RelationshipType["ManyToMany"] = 1] = "ManyToMany";
-    RelationshipType[RelationshipType["ManyToOne"] = 2] = "ManyToOne";
-})(exports.RelationshipType || (exports.RelationshipType = {}));
-var RelationshipType = exports.RelationshipType;
-;
-;
