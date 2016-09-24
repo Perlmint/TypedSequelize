4e6fa30e574582e466901b9d3458ade682c6e159
Gyusun Yeom <omniavinco@gmail.com>
2016-09-24 11:17:26 +0900

    bin

---
+++ bin/util.js
@@ -0,0 +1,33 @@
+"use strict";
+/// <reference path="../typings/index.d.ts" />
+const ts = require("typescript");
+function tsTypeToString(t, associated = false) {
+    var typename = _tsTypeToString(t);
+    if (associated) {
+        typename = typename.replace(/(\[])?$/, "Interface$&");
+    }
+    return typename;
+}
+exports.tsTypeToString = tsTypeToString;
+function _tsTypeToString(t) {
+    if (t.flags & ts.TypeFlags.String) {
+        return "string";
+    }
+    if (t.flags & ts.TypeFlags.Number) {
+        return "number";
+    }
+    if (t.flags & ts.TypeFlags.Boolean) {
+        return "boolean";
+    }
+    switch (t.getSymbol().name) {
+        case "Date":
+            return "Date";
+        case "Buffer":
+            return "Buffer";
+        case "Array":
+            let arrayt = t;
+            return tsTypeToString(arrayt.typeArguments[0]) + "[]";
+        default:
+            return t.getSymbol().name;
+    }
+}
